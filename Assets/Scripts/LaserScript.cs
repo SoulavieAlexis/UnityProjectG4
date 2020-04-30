7780d6c7b35760049d85a7acebeb8baadee67cd7
Tumasgiu <tumasgiu@users.noreply.github.com>
2020-04-30 14:02:17 +0200

    Player
    Tout ce qui conerne la gestion du player, ses tirs ainsi que la récupération d'item de pouvoir

---
+++ Assets/Scripts/LaserScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserScript : MonoBehaviour{
+
+    public LineRenderer monLinerRender;
+    public float tailleLaser;
+
+    // Variable collision item
+    public bool isPick = false;
+
+    // Start is called before the first frame update
+    void Start(){
+        monLinerRender = GetComponent<LineRenderer>();
+        monLinerRender.enabled = false;
+    }
+    // Update is called once per frame
+    void Update(){
+        RaycastHit2D rayonLaser = Physics2D.Raycast(transform.position, transform.up, tailleLaser);
+
+        if(Input.GetButton("Fire1")){
+
+            monLinerRender.enabled = true;
+
+            if(rayonLaser == true){
+                monLinerRender.SetPosition(0, transform.position);
+                monLinerRender.SetPosition(1, new Vector3(transform.position.x, transform.position.y + tailleLaser, transform.position.z));
+            }else{
+                Vector3 finLaser = new Vector3(transform.position.x, transform.position.y + tailleLaser, 0);
+
+                monLinerRender.SetPosition(0, transform.position);
+                monLinerRender.SetPosition(0, finLaser);
+            }
+        }else{
+            monLinerRender.enabled = false;
+        }
+    }
+}
---
+++ Assets/Scripts/PickUpObjectScript.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class PickUpObjectScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public bool isPick = false;
+
+    private void OnTriggerEnter2D(Collider2D collision){
+        if(collision.CompareTag("Player")){
+            isPick = true;
+            Destroy(gameObject);
+        }
+    }
+}
