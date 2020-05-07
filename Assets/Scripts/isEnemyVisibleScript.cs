6026a82dec421343c5e9226a8996049167952512
Tumasgiu <tumasgiu@users.noreply.github.com>
2020-05-07 13:35:21 +0200

    Ajustement de l'ajustement du bug des item
    Je suis perfectionniste

--- Assets/Scripts/isEnemyVisibleScript.cs
+++ Assets/Scripts/isEnemyVisibleScript.cs
@@ -18,12 +18,10 @@ public class isEnemyVisibleScript : MonoBehaviour
         if (m_Renderer.isVisible)
         {
             GetComponent<HealthScript>().isEnemy = true; 
-            Debug.Log(GetComponent<HealthScript>().isEnemy);
         }
         else
         {
             GetComponent<HealthScript>().isEnemy = false;
-            Debug.Log(GetComponent<HealthScript>().isEnemy);
         }
         
         
