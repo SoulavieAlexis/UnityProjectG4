0b9c0fcf5ac9b180764975b930e650ee57d28eba
tasnim99500 <56479878+tasnim99500@users.noreply.github.com>
2020-05-09 09:21:28 +0200

    go to level 2
    passer à un deuxieme  niveau , nouvelle scéne

---
+++ Assets/Scripts/LoadLevel.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+public class LoadLevel : MonoBehaviour {
+    public string levelname= "enter level name here";
+    private void OnTrigerEnter2D(Collider2D other) {
+      
+      if (other.tag == "player")
+      {
+          Application.LoadLevel(levelname);
+      }
+    }
+}
+ 
